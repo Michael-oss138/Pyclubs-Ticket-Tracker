98db714121ff7768b157bb0fad7cb580a423242e
Michael-oss138 <nwaizumichael0@gmail.com>
2025-11-29 23:05:25 +0100

    event selection view

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import TicketSelection
+from events.models import Event
+from  django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def select_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+
+
+    TicketSelection.objects.get_or_create(
+        user = request.user
+        event=event
+    )
+        
+    return redirect("my_events")
