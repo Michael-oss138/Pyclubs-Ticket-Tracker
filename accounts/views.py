6cbc48c37471a697569ff4de3c09d2b15de4b303
Michael-oss138 <nwaizumichael0@gmail.com>
2025-12-04 23:30:48 +0100

    the register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        confirm_password=request.POST.get('confirm_password')
+
+        if password !-= confirm_password:
+            messages.error(request, "Passwords do not match")
+            return redirect("register")
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exosts")
+            return redirect("register")
+
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        messages.success(equest, "Account created successfully")
+        return redirect("login")
+    return render(request, "register.html")
--- ticket_tracker/urls.py
+++ ticket_tracker/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', include('events.urls')),
     path('tickets/', include('tickets.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
