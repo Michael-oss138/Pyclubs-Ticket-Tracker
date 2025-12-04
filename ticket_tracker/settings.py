1bcc2cf43604f187bd7c4d71ca856515c92bf5d7
Michael-oss138 <nwaizumichael0@gmail.com>
2025-12-04 23:11:55 +0100

    a basic template that displays events

--- ticket_tracker/settings.py
+++ ticket_tracker/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ticket_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
