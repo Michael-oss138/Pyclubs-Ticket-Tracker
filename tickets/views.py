01db148d7a49e2d6c2895344601849bec5f39492
Michael-oss138 <nwaizumichael0@gmail.com>
2025-12-18 14:48:59 +0100

    capacity check

--- tickets/views.py
+++ tickets/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import TicketSelection
 from events.models import Event
-from  django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 @login_required
 def select_event(request, event_id):
     event = get_object_or_404(Event, id=event_id)
 
+    if TicketSelection.objects.filter(user=request.user, event=event).exists():
+        messages.warning(request, "You are already registered for this event.")
+        return redirect("my_events")
 
-    TicketSelection.objects.get_or_create(
-        user = request.user,
+    if TicketSelection.objects.filter(event=event).count() >= event.capacity:
+        messages.error(request, "This event is fully booked.")
+        return redirect("my_events")
+
+    TicketSelection.objects.create(
+        user=request.user,
         event=event
     )
-        
+    
+    messages.success(request, "You have successfully registered for the event!")
     return redirect("my_events")
 
+
+@login_required
 def my_events(request):
     selections = TicketSelection.objects.filter(user=request.user)
     return render(request, "tickets/my_events.html", {"selections": selections})
