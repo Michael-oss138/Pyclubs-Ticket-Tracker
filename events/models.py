d3cd7c5d978e47553997ae53efbac580e11dce24
Michael-oss138 <nwaizumichael0@gmail.com>
2025-12-18 14:41:08 +0100

    done

--- events/models.py
+++ events/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 
+
 class Semester(models.Model):
     name = models.CharField(max_length=100)
     start_date = models.DateField()
--- tickets/models.py
+++ tickets/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from events.models import Event
 
-# Create your models here.
-
 class TicketSelection(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ticket_selections")
-    event = models.ForeignKey(User, on_delete=models.CASCADE, related_name="event_selections")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="event_selections")
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ('user', 'event')  # prevents double registration
+
     def __str__(self):
-        return self.user.username + " - " + self.event.title
+        return f"{self.user.username} - {self.event.title}"
